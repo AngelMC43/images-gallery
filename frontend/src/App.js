3d718df728ea412031b21284e9b56835e09e4ff1
Angel MC <angelmucaba@gmail.com>
2022-06-22 11:49:38 +0200

    search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+
+const UNSPLAH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 function App() {
+  const [word, setWord] = useState("");
+
+  const handleSearchSubmit = (e) => {
+    e.prevent.default();
+    console.log(word);
+    // fetch(
+    //   `https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLAH_KEY}`
+    // )
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log(data).catch((err) => {
+    //       console.log(err);
+    //     });
+    //   });
+  };
+
   return (
-    <div className="App">
-      <h1>Images Gallery</h1>
+    <div>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
