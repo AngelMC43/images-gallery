b50158fc717e4014f8b4ea2c7ec52c524eff9692
Angel MC <angelmucaba@gmail.com>
2022-06-23 15:52:35 +0200

    Welcome component

---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,21 @@
+import { Jumbotron, Button } from "react-bootstrap";
+
+const Welcome = () => {
+  return (
+    <div>
+      <div className="m-5 p-5">
+        <h1>Search an image</h1>
+        <p>
+          This is a simple application tha retrieves photos using Unplash API.
+        </p>
+        <p>
+          <Button variant="primary" href="https://unsplash.com" target="_blank">
+            Learn more
+          </Button>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
