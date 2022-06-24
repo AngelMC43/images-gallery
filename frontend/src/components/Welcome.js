48a0646972aea023119efb7c58549c3a57c75bf0
Angel MC <angelmucaba@gmail.com>
2022-06-24 13:49:44 +0200

    python installation virtual enviroment

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -3,13 +3,13 @@ import { Jumbotron, Button } from "react-bootstrap";
 const Welcome = () => {
   return (
     <div>
-      <div className="m-5 p-5">
+      <div className="m-5 p-5 bg-black text-light">
         <h1>Search an image</h1>
         <p>
           This is a simple application tha retrieves photos using Unplash API.
         </p>
         <p>
-          <Button variant="primary" href="https://unsplash.com" target="_blank">
+          <Button variant="info" href="https://unsplash.com" target="_blank">
             Learn more
           </Button>
         </p>
